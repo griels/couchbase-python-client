5478be9683e5283f6271a46f1d62d0b7ad7b3f16
Ellis Breen <ellis.breen@couchbase.com>
2017-10-20 18:05:59 +0100

    Disable enhanced error test with RBAC/subdoc on mock as doesn't appear to be enabled by default on mock.

--- couchbase/tests/cases/enh_err_t.py
+++ couchbase/tests/cases/enh_err_t.py
@@ -7,20 +7,29 @@ import unittest
 
 from couchbase.exceptions import ( CouchbaseError )
 from couchbase.tests.base import MockTestCase, ConnectionTestCase,\
-    CouchbaseTestCase
+    CouchbaseTestCase, RealServerTestCase
 from couchbase.auth_domain import AuthDomain
 import sys
 import couchbase
 import logging
+import time
+from nose import SkipTest
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 couchbase.enable_logging()
 
 class EnhancedErrorTest(CouchbaseTestCase):
-
     def setUp(self):
         super(EnhancedErrorTest, self).setUp()
+
+        if not self._realserver_info:
+            raise SkipTest("Need real server")
+        
         self.admin = self.make_admin_connection()
 
+    @property
+    def cluster_info(self):
+        return self.realserver_info
+    
     def tearDown(self):
         super(EnhancedErrorTest, self).tearDown()
         if self.should_check_refcount:
@@ -32,7 +41,7 @@ class EnhancedErrorTest(CouchbaseTestCase):
 
     def test_enhanced_err_present_authorisation(self):
         import couchbase.subdocument as SD
-        users=[#('writer',('s3cr3t',[('data_reader', 'default'), ('data_writer', 'default')])),
+        users=[('writer',('s3cr3t',[('data_reader', 'default'), ('data_writer', 'default')])),
               ('reader',('s3cr3t',[('data_reader', 'default')]))]
         #self.mockclient._do_request("SET_ENHANCED_ERRORS",{"enabled":True})
         for user in users:
@@ -40,6 +49,7 @@ class EnhancedErrorTest(CouchbaseTestCase):
             (userid, password, roles) = user[0],user[1][0],user[1][1]
             # add user
             self.admin.user_upsert(AuthDomain.Local, userid, password, roles)
+            time.sleep(1)
             try:
                 connection = self.make_connection(username=userid,password=password)
                 
@@ -49,6 +59,7 @@ class EnhancedErrorTest(CouchbaseTestCase):
                 #rv = connection.upsert(self.gen_key(), 'value1')
                 # get all users
             except CouchbaseError as e:
+                print str(e)
                 if userid=="writer":
                     raise e
                 else:
@@ -56,9 +67,9 @@ class EnhancedErrorTest(CouchbaseTestCase):
                     self.assertRegexpMatches(e.ref,r"(.*?)-(.*?)-.*","doesn't have correct Ref field")
                     self.assertRegexpMatches(str(e),r".*Context=Authorization failure.*,.*Ref=.*","exception as string doesn't contain both fields")
             finally:
-                pass
+                #pass
             #remove user
-            #    self.admin.user_remove(AuthDomain.Local, userid)
+                self.admin.user_remove(AuthDomain.Local, userid)
             
         pass
 
