6d5a9b3e5ee6136a7e55de3bb2972adc0d2748cf
Ellis Breen <spacestation9@gmail.com>
2017-10-23 19:17:43 +0100

    Get rid of tabs.
    Change-Id: Iecd96bcd442f3a3a391ca6a76d2de70bc2c48598

--- src/callbacks.c
+++ src/callbacks.c
@@ -96,7 +96,7 @@ operation_completed3(pycbc_Bucket *self, pycbc_MultiResult *mres, pycbc_enhanced
     if (mres)
     {
         mres->err_info=err_info;
-		Py_XINCREF(err_info);
+        Py_XINCREF(err_info);
     }
     if ((self->flags & PYCBC_CONN_F_ASYNC) == 0) {
         if (!self->nremaining) {
@@ -153,7 +153,7 @@ operation_completed_with_err_info(pycbc_Bucket *self, pycbc_MultiResult *mres, i
     pycbc_enhanced_err_info* err_info;
     pycbc_enhanced_err_info_store(&err_info,resp,cbtype);
     operation_completed3(self,mres,err_info);
-	Py_XDECREF(err_info);
+    Py_XDECREF(err_info);
 }
 /**
  * Call this function for each callback. Note that even if this function
