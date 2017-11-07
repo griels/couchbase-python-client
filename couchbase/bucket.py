4a67cb7d9427749aad61e15083b2e93b9dbdcc87
Ellis Breen <ellis.breen@couchbase.com>
2017-11-07 11:38:14 +0000

    PYCBC-412: add health check function into lcb check
    Motivation
    ----------
    Provide a convenient method of checking the health of a cluster
    without requiring client knowledge of the network configuration.
    
    Modifications
    -------------
    - Use the uncommitted Health Check API (i.e. lcb_ping3)
    from libcouchbase to ping the nodes in a cluster and get
    latency/status/type information
    - Marshal the data from the accompanying callback into both
    'services_struct' (from the resp->services field) and
    'services_json' (from the resp->json field). Struct version
    populates latency as a number rather than a string.
    
    Results
    -------
    Client now successfully passes through the healthcheck
    information from the 'services_struct' dictionary.
    'services_json' dictionary is marshalled if present,
    to allow quick adaptation in case of changes in lcb_ping3
    behaviour.
    
    Change-Id: I44b3b96c18c6e2a768a3ab9770bea0fc1a244e33

--- couchbase/bucket.py
+++ couchbase/bucket.py
@@ -34,7 +34,6 @@ from couchbase._pyport import basestring
 import couchbase.subdocument as SD
 import couchbase.priv_constants as _P
 
-
 ### Private constants. This is to avoid imposing a dependency requirement
 ### For simple flags:
 
@@ -906,6 +905,26 @@ class Bucket(_Base):
         if keys and not isinstance(keys, (tuple, list)):
             keys = (keys,)
         return self._stats(keys, keystats=keystats)
+
+    def get_health(self):
+        """Request cluster health information.
+
+        Fetches health information from each node in the cluster. 
+        It returns a `dict` with 'type' keys
+        and server summary lists as a value.
+
+
+        :raise: :exc:`.CouchbaseNetworkError`
+        :return: `dict` where keys are stat keys and values are
+            host-value pairs
+
+        Get health info (works on couchbase buckets)::
+
+            cb.get_health()
+            # {'services': {...}, ...}
+        """
+        resultdict = self._get_health()
+        return resultdict['services_struct']
 
     def observe(self, key, master_only=False):
         """Return storage information for a key.
