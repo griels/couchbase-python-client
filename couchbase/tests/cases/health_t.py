c00303b5f9236ede9305026648276270150b7ce0
Ellis Breen <ellis.breen@couchbase.com>
2017-12-21 18:18:02 +0000

    Fix tests.
    Change-Id: I7338ffc740f284b05e705a968ed0805df394b53c

--- couchbase/tests/cases/health_t.py
+++ couchbase/tests/cases/health_t.py
@@ -24,62 +24,49 @@ try:
 except NameError:
     basestring = str
 
+server_schema = {"type": "object",
+                 "properties": {"details": {"type": "string"},
+                                "latency": {"anyOf": [{"type": "number"}, {"type": "string"}]},
+                                "server": {"type": "string"},
+                                "status": {"type": "number"}
+                                },
+                 "required": ["details", "latency", "server", "status"]}
+
+servers_schema = {"type": "array",
+                  "items": server_schema}
+
+
+def gen_schema(name):
+    return {"type": "object",
+            "properties": {name: servers_schema},
+            "required": [name]
+            }
+
+
+services_schema = {"anyOf":
+                       [gen_schema(name) for name in ["n1ql", "views", "fts", "kv"]]
+                   }
+health_schema = {"anyOf": [{
+    "type": "object",
+    "properties": {
+        "services": services_schema
+    },
+    "required": ["services"]
+}]}
+
 
 class HealthTest(ConnectionTestCase):
+
     def setUp(self):
         super(HealthTest, self).setUp()
         self.skipUnlessMock()
 
-    server_schema = {"type": "object",
-                     "properties": {"details": {"type": "string"},
-                                    "latency": {"anyOf": [{"type": "number"}, {"type": "string"}]},
-                                    "server": {"type": "string"},
-                                    "status": {"type": "number"}
-                                    },
-                     "required": ["details", "latency", "server", "status"]}
-
-    servers_schema = {"type": "array",
-                      "items": server_schema}
-
-    @staticmethod
-    def gen_schema(name):
-        return {"type": "object",
-                "properties": {name: HealthTest.servers_schema},
-                "required": [name]
-                }
-
     def test_ping(self):
         result = self.cb.ping()
-
-        services_schema = {"anyOf":
-                               [HealthTest.gen_schema(name) for name in ["n1ql", "views", "fts", "kv"]]
-                           }
-
-        health_schema = {"anyOf": [{
-            "type": "object",
-            "properties": {
-                "services": services_schema
-            },
-            "required": ["services"]
-        }]}
-
         jsonschema.validate(result, services_schema)
 
     def test_diagnostics(self):
         result = self.cb.diagnostics()
-
-        services_schema = {"anyOf":
-                               [HealthTest.gen_schema(name) for name in ["n1ql", "views", "fts", "kv"]]
-                           }
-
-        health_schema = {"anyOf": [{
-            "type": "object",
-            "properties": {
-                "services": services_schema
-            },
-            "required": ["services"]
-        }]}
-
         jsonschema.validate(result, services_schema)
 
 if __name__ == '__main__':
