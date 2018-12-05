7a87fe13d0b4b259eb3df7b4375b8447f809b501
Ellis Breen <ellis.breen@couchbase.com>
2018-12-05 09:54:21 +0000

    Hybrid build
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: I924fbaed4a83801e88b43ce1528764bbd8b97154
    (cherry picked from commit 06cc9bd59d517eec664a63a52c4334b8dc894e6a) (+2 squashed commits)
    Squashed commits:
    [22b7aa5] Fix install
    
    Change-Id: Iab3e8ed46c8147dea9da89137446048a1eefb289
    [8373560] PYCBC-546: Update CMake config to pull in deps
    
    Change-Id: I835e5be8fae4bc3298ce80a72a735fea21735dd9
    (cherry picked from commit 806c08d9a9944ab03b8632c41c2c819722de7fcc)
    (cherry picked from commit a6413f1befeef209e4134d351927f8e9bc878eff)

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import logging
 import sys
 import os.path
 import os
@@ -6,6 +7,8 @@ import platform
 import warnings
 import couchbase_version
 import pip
+
+from cmodule import module, extoptions, CPP_BUILD
 
 try:
     if os.environ.get('PYCBC_NO_DISTRIBUTE'):
@@ -16,7 +19,6 @@ except ImportError:
     from distutils.core import setup, Extension
 
 import re
-extoptions = {}
 pkgdata = {}
 pkgversion = None
 
@@ -49,21 +51,21 @@ extoptions['extra_link_args'] = []
 
 
 def boolean_option(flag):
-    return "-D{}={}".format(flag,os.environ.get(flag))
+    return ["-D{}={}".format(flag,os.environ.get(flag))]
 
 
 COMP_OPTION_PREFIX = "PYCBC_COMP_OPT_"
 
 
 def comp_option(flag):
-    return "-{}={}".format(flag.replace(COMP_OPTION_PREFIX, ""), os.environ.get(flag))
+    return ["-{}={}".format(flag.replace(COMP_OPTION_PREFIX, ""), os.environ.get(flag))]
 
 
 COMP_OPTION_BOOL_PREFIX = "PYCBC_COMP_OPT_BOOL_"
 
 
 def comp_option_bool(flag):
-    return "-{}".format(flag.replace(COMP_OPTION_BOOL_PREFIX, ""))
+    return ["-{}".format(flag.replace(COMP_OPTION_BOOL_PREFIX, ""))]
 
 
 CLANG_SAN_OPTIONS={"address":"lsan","undefined":"ubsan"}
@@ -72,9 +74,9 @@ CLANG_SAN_PREFIX = "PYCBC_SAN_OPT_"
 
 def comp_clang_san_option(flag):
     san_option = flag.replace(CLANG_SAN_PREFIX, "")
-    fsanitize_statement = "-fsanitize={}".format(san_option)
-    extoptions['extra_link_args']+=["{}".format(fsanitize_statement)]
-    return fsanitize_statement
+    fsanitize_statements = ["-fsanitize={}".format(san_option),"-fno-omit-frame-pointer"]
+    extoptions['extra_link_args']+=fsanitize_statements+['-Llibclang_rt.asan_osx_dynamic']
+    return fsanitize_statements
 
 
 def comp_option_pattern(prefix):
@@ -90,19 +92,35 @@ comp_flags = {"PYCBC_STRICT":boolean_option,
               comp_option_pattern(CLANG_SAN_PREFIX):comp_clang_san_option}
 
 debug_symbols = len(set(os.environ.keys()) & {"PYCBC_DEBUG", "PYCBC_DEBUG_SYMBOLS"}) > 0
-comp_arg_additions = (action(actual_flag) for flag, action in comp_flags.items() for actual_flag in os.environ.keys() if
+import itertools,functools
+comp_arg_additions = itertools.chain.from_iterable(action(actual_flag) for flag, action in comp_flags.items() for actual_flag in os.environ.keys() if
                       re.match(flag, actual_flag))
-extoptions['extra_compile_args'] += comp_arg_additions
+
+print(comp_arg_additions)
+
+extoptions['include_dirs']=[]
+
+from cmake_build import CMakeBuildInfo
+
+extoptions['extra_compile_args'] += list(comp_arg_additions)
+from cmake_build import CMakeBuild
+build_info=CMakeBuildInfo()
+build_info.pkgdata=pkgdata
+
+if CPP_BUILD:
+    extoptions['extra_compile_args'] += ['-std=c++1y']
 if sys.platform != 'win32':
-    extoptions['libraries'] = ['couchbase']
+    extoptions['extra_compile_args'] += ['-Wno-strict-prototypes', '-fPIC']
+    extoptions['libraries'] = ['couchbase'] + (['boost_python27'] if CPP_BUILD else [])
     if debug_symbols:
         extoptions['extra_compile_args'] += ['-O0', '-g3']
         extoptions['extra_link_args'] += ['-O0', '-g3']
     if sys.platform == 'darwin':
         warnings.warn('Adding /usr/local to search path for OS X')
-        extoptions['library_dirs'] = ['/usr/local/lib']
+        extoptions['library_dirs'] = ['/usr/local/lib',
+                                      '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/10.0.0/lib/darwin/']
         extoptions['include_dirs'] = ['/usr/local/include']
-
+    print(pkgdata)
 else:
     if sys.version_info<(3,0,0):
         if pip.__version__<"9.0.0":
@@ -137,48 +155,7 @@ else:
     pkgdata['couchbase'] = ['libcouchbase.dll']
 
 
-SOURCEMODS = [
-        'exceptions',
-        'ext',
-        'result',
-        'opresult',
-        'callbacks',
-        'cntl',
-        'convert',
-        'bucket',
-        'store',
-        'constants',
-        'multiresult',
-        'miscops',
-        'typeutil',
-        'oputil',
-        'get',
-        'counter',
-        'http',
-        'htresult',
-        'ctranscoder',
-        'crypto',
-        'observe',
-        'iops',
-        'connevents',
-        'pipeline',
-        'views',
-        'n1ql',
-        'fts',
-        'ixmgmt'
-        ]
 
-if platform.python_implementation() != 'PyPy':
-    extoptions['sources'] = [ os.path.join("src", m + ".c") for m in SOURCEMODS ]
-    module = Extension('couchbase._libcouchbase', **extoptions)
-    setup_kw = {'ext_modules': [module]}
-else:
-    warnings.warn('The C extension libary does not work on PyPy. '
-            'You should install the couchbase_ffi module. Installation of this '
-            'module will continue but will be unusable without couchbase_ffi')
-    setup_kw = {}
-
-cmake_build=os.environ.get("PYCBC_CMAKE_BUILD")
 
 # Dummy dependency to prevent installation of Python < 3 package on Windows.
 
@@ -187,7 +164,83 @@ pip_not_on_win_python_lt_3 = (
     if pip.__version__ >= "9.0.0"
     else [])
 
-conan_and_cmake_deps = (['conan', 'cmake>=3.0.2'] if
+
+def cppyy_deps(deps):
+    return {"dependency_links": list(map(
+        lambda dep: u'https://bitbucket.org/wlav/{}/get/master.tar.bz2'.format(str.lower(dep)), deps))} if "linux" in platform.system().lower() else {
+        "requires":deps
+    }
+
+
+cppyy_build = os.environ.get("PYCBC_CPPYY")
+deps = cppyy_deps(['cppyy-backend', 'cppyy']) if cppyy_build else {}
+typing_requires = (['typing'] if sys.version_info < (3, 7) else [])
+general_requires = ['pyrsistent',"enum34; python_version < '3.5'"] + deps.pop("requires", [])
+dependency_links = deps.pop("dependency_links", [])
+
+from distutils.command.install_headers import install_headers as install_headers_orig
+
+import os
+import re
+import sys
+import platform
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+
+class install_headers(install_headers_orig):
+
+    def run(self):
+        headers = self.distribution.headers or []
+        for header in headers:
+            dst = os.path.join(self.install_dir, os.path.dirname(header))
+            self.mkpath(dst)
+            (out, _) = self.copy_file(header, dst)
+            self.outfiles.append(out)
+
+build_type = os.getenv("PYCBC_BUILD",
+                       {"Windows": "CMAKE", "Darwin": "CMAKE_HYBRID", "Linux": "CMAKE_HYBRID"}.get(platform.system(),
+                                                                                                   "CMAKE_HYBRID"))
+cmake_build=build_type in ['CMAKE','CMAKE_HYBRID']
+print("Build type: {}, cmake:{}".format(build_type,cmake_build))
+
+if cmake_build:
+    from cmake_build import CMakeExtension, CMakeBuild, CMakeBuildInfo
+
+    CMakeBuild.hybrid=build_type in ['CMAKE_HYBRID']
+    CMakeBuild.info=build_info
+    b_ext = CMakeBuild
+    e_mods=[CMakeExtension('couchbase._libcouchbase','',**extoptions)]
+    general_requires+=['cmake']
+else:
+    print("Legacy build")
+    b_ext = build_ext
+    e_mods=[module]
+
+setup_kw = {'ext_modules': e_mods}
+logging.error(setup_kw)
+
+
+exec_requires = typing_requires + general_requires
+
+
+def setup_cxx_headers():
+    def cxx_includes_base():
+        yield os.path.join(*(build_info.base+['libcouchbase-cxx-prefix/src/libcouchbase-cxx/include/']))
+    cxx_includes = lambda base: base+['/libcouchbase-cxx-prefix/src/libcouchbase-cxx/include/libcouchbase/']
+    cxx_includes_inner = lambda cxx_includes: cxx_includes+['couchbase++']
+    import glob
+    #all_headers = ([os.path.join(cxx_includes, 'couchbase++.h')] +
+    #               glob.glob(os.path.join(cxx_includes_inner, "*.h"))) if cppyy_build else []
+    extoptions['include_dirs'] = itertools.chain(extoptions['include_dirs'],cxx_includes_base)
+
+
+setup_cxx_headers()
+
+conan_build=os.environ.get("PYCBC_CONAN_BUILD")
+conan_deps = ['conan'] if conan_build else []
+conan_and_cmake_deps = ((['scikit-build','cmake>=3.0.2']+conan_deps) if
                         cmake_build and sys.platform.startswith('darwin') else [])
 
 setup(
@@ -217,6 +270,7 @@ setup(
     packages = [
         'acouchbase',
         'couchbase',
+        'couchbase.v3',
         'couchbase.views',
         'couchbase.iops',
         'couchbase.asynchronous',
@@ -230,9 +284,11 @@ setup(
         'acouchbase.py34only'
     ] if sys.version_info >= (3, 4) else []),
     package_data=pkgdata,
-    setup_requires=['typing'] + conan_and_cmake_deps,
-    install_requires=['typing'] + pip_not_on_win_python_lt_3,
-    tests_require=['nose', 'testresources>=0.2.7', 'basictracer==2.2.0'],
+    setup_requires=exec_requires + conan_and_cmake_deps,
+    install_requires=exec_requires + pip_not_on_win_python_lt_3,
+    dependency_links=dependency_links,
+    tests_require=['utilspie','nose', 'testresources>=0.2.7', 'basictracer==2.2.0'],
     test_suite='couchbase.tests.test_sync',
+    cmdclass={'install_headers': install_headers, 'build_ext':b_ext},
     **setup_kw
 )
