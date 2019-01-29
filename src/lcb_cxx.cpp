b592fddca23b390ddae42e2d1dde6d8bf443997c
Ellis Breen <ellis.breen@couchbase.com>
2019-01-29 20:14:55 +0000

    Collections support
    Change-Id: I6053c26cf8cc9ade78b63ec89bfd425d1ae35e60

---
+++ src/lcb_cxx.cpp
@@ -0,0 +1,32 @@
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <functional>
+#include <string>
+
+#include <pybind11/pybind11.h>
+
+typedef std::function< pybind11::module & (std::string const &) > ModuleGetter;
+
+
+
+PYBIND11_MODULE(lcb_cxx, root_module) {
+	root_module.doc() = "lcb_cxx module";
+
+	std::map <std::string, pybind11::module> modules;
+	ModuleGetter M = [&](std::string const &namespace_) -> pybind11::module & {
+		auto it = modules.find(namespace_);
+		if( it == modules.end() ) throw std::runtime_error("Attempt to access pybind11::module for namespace " + namespace_ + " before it was created!!!");
+		return it->second;
+	};
+
+	modules[""] = root_module;
+
+	std::vector< std::pair<std::string, std::string> > sub_modules {
+	};
+	for(auto &p : sub_modules ) modules[p.first.size() ? p.first+"::"+p.second : p.second] = modules[p.first].def_submodule(p.second.c_str(), ("Bindings for " + p.first + "::" + p.second + " namespace").c_str() );
+
+	//pybind11::class_<std::shared_ptr<void>>(M(""), "_encapsulated_data_");
+
+
+}
