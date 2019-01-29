42b6396eaec2e10354612e4f635a46b5c075e57e
Ellis Breen <ellis.breen@couchbase.com>
2019-01-29 20:14:55 +0000

    Made tracing switchable again
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: I5c208477aa18e15fcc53828c2d67b6adecd29536
    (cherry picked from commit 180f4b0b036a3a8cf9ef2765efeb164d19ddb22f) (+1 squashed commit)
    Squashed commits:
    [300aac4] Implement V4 API and V4 backport to V3
    
    Factor out subdocops
    
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: Ifccc5882c2cdc47490688415dc69b5705114a250
    (cherry picked from commit 70a3819dc190839b1c8c7a3fe1b7bfff00f72068) (+1 squashed commit)
    Squashed commits:
    [d7743b4] Fix counter stuff first
    
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    he commit message for your changes. Lines starting
    
    Change-Id: I45d86b378f52fe74574dc9d3802a695392547190 (+1 squashed commit)
    Squashed commits:
    [1887466] Logging tweaks
    
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: If261df17d4c0e9fa18219ca9fc166252453f9fdf (+8 squashed commits)
    Squashed commits:
    [aa3d8cf] V4 backport working again, try switching in V4 again.
    
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: I75e373af6a59fd5caeda72938836d07b6e64b019 (+1 squashed commit)
    Squashed commits:
    [62a2be5] Hopefully final final remerge
    
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: Ia374cb4c51f767b1f4a4f8128c4f9bbaef8207e0
    [a26445f] Nearly there with final remerge
    
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: I6dae3ff50276eb8dbbc39ead3b9c3118877764f7
    [c783146] V4.1 API Adaptations
    
    Change-Id: Iebdbe654296edf758f49ef65c6c388315fa86a84
    (cherry picked from commit b2697467dceaa3738dc1d32b151b55e36ab3391a) (+1 squashed commit)
    Squashed commits:
    [6e2f729] Readding features now 3.7 stabilized, post collection fixes
    
    Change-Id: I522cec8c728bbfa725da3bed793a1e7711b7c1d3
    [bef7acb] V4 feature merge
    
    SDKJENKINS_PIP_INSTALL
    
    Change-Id: I9d48fc7197990c37f604e766f550a18163b4c392
    (cherry picked from commit 6032f70c9089b4370312490403d1b72bb8e2a3e6) (+1 squashed commit)
    Squashed commits:
    [ad8631f] V4 adaptations merge
    
    SDKJENKINS_PIP_INSTALL
    
    Change-Id: I1b5bc550c46fa193a5469a45af20f44759e38545
    (cherry picked from commit 2efc73a9d59d5a759474933c105edc67798b5206)
    [8464119] More V4/collections work
    
    Change-Id: I8cb1fd99a9229dbeb63f50a72e73596d145ad695
    [afe2b0b] PYCBC-564: Sychronous replication, V3 style
    
    (cherry picked from commit 89b7a69c7ea92970999377128910eee847a0efa5)
    
    Change-Id: Ieb7377fde3bf1c253dc8dc58a59ac5c9cab551a6
    (cherry picked from commit 27fae4d5897773a6e775bff3798c2289b6e44993)
    [19ff644] Collections support MK2
    
    Change-Id: I05db3020b27bafacab7a4907c013344933710efb
    [b592fdd] Collections support
    
    Change-Id: I6053c26cf8cc9ade78b63ec89bfd425d1ae35e60

---
+++ src/pycbc_subdocops.c
@@ -0,0 +1,141 @@
+/**
+ *     Copyright 2019 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ **/
+
+#include "pycbc_subdocops.h"
+pycbc_sd_metainfo pycbc_get_metainfo(pycbc_sdspec_details details)
+{
+    pycbc_sd_metainfo result = {0};
+#define PYCBC_METAINFO_PATH_ONLY(UC, LC, ...) \
+    result = (pycbc_sd_metainfo){.is_multival = 0, .category = PYCBC_PATH_ONLY};
+#define PYCBC_METAINFO_COUNTER(UC, LC, ...) \
+    result = (pycbc_sd_metainfo){.is_multival = 0, .category = PYCBC_COUNTER};
+#define PYCBC_METAINFO_NP(UC, LC, ...) \
+    result = (pycbc_sd_metainfo){.is_multival = 0, .category = PYCBC_NIL};
+#define PYCBC_METAINFO_VAL_GEN(UC, LC, ...) \
+    result = (pycbc_sd_metainfo){.is_multival = 0, .category = PYCBC_STR};
+#define PYCBC_METAINFO_IS_MVAL(UC, LC, ...) \
+    result = (pycbc_sd_metainfo){.is_multival = 1, .category = PYCBC_STR};
+
+#define PYCBC_METAINFO_SDCMD_CASE(UC, LC, ...) \
+    PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_METAINFO_PATH_ONLY, __VA_ARGS__)
+#define PYCBC_METAINFO_SDCMD_CASE_NP(UC, LC, ...) \
+    PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_METAINFO_NP, __VA_ARGS__)
+#define PYCBC_METAINFO_SDCMD_CASE_VAL(UC, LC, ...) \
+    PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_METAINFO_VAL_GEN, __VA_ARGS__)
+#define PYCBC_METAINFO_SDCMD_CASE_MVAL(UC, LC, ...) \
+    PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_METAINFO_IS_MVAL, __VA_ARGS__)
+#define PYCBC_METAINFO_SDCMD_CASE_COUNTER(UC, LC, ...) \
+    PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_METAINFO_COUNTER, __VA_ARGS__)
+    switch (details.op) {
+        PYCBC_X_SD_OPS(PYCBC_METAINFO_SDCMD_CASE,
+                       PYCBC_METAINFO_SDCMD_CASE_NP,
+                       PYCBC_METAINFO_SDCMD_CASE_VAL,
+                       PYCBC_METAINFO_SDCMD_CASE_MVAL,
+                       PYCBC_METAINFO_SDCMD_CASE_COUNTER)
+        default:
+            result.err = LCB_SUBDOC_PATH_EINVAL;
+            break;
+    }
+    result.has_valbuf = (result.category == PYCBC_STR);
+    return result;
+#undef PYCBC_METAINFO_PATH_ONLY
+#undef PYCBC_METAINFO_COUNTER
+#undef PYCBC_METAINFO_NP
+#undef PYCBC_VAL_GEN
+#undef PYCBC_METAINFO_IS_MVAL
+
+#undef PYCBC_METAINFO_SDCMD_CASE
+#undef PYCBC_METAINFO_SDCMD_CASE_NP
+#undef PYCBC_METAINFO_SDCMD_CASE_VAL
+#undef PYCBC_METAINFO_SDCMD_CASE_MVAL
+#undef PYCBC_METAINFO_SDCMD_CASE_COUNTER
+};
+
+pycbc_sdspec pycbc_build_spec(lcb_SUBDOCOPS *subdocops,
+                                     pycbc_sdspec_details details)
+{
+    pycbc_sdspec result = {0};
+
+#if 1 || PYCBC_LCB_API > 0x030000
+#    define PYCBC_BUILDSPEC_PATH_ONLY(UC, LC, ...)            \
+        lcb_subdocops_##LC(subdocops,               \
+                           details.index,           \
+                           details.flags,           \
+                           details.pathbuf->buffer, \
+                           details.pathbuf->length);
+#    define PYCBC_BUILDSPEC_COUNTER(UC, LC, ...)                                 \
+        lcb_subdocops_##LC(                                            \
+                subdocops,                                             \
+                details.index,                                         \
+                details.flags,                                         \
+                details.pathbuf->buffer,                               \
+                details.pathbuf->length,                               \
+                (details.valbuf && details.valbuf->buffer)             \
+                        ? strtol((const char *)details.valbuf->buffer, \
+                                 NULL,                                 \
+                                 10)                                   \
+                        : 0);
+#    define PYCBC_BUILDSPEC_NP(UC, LC, ...) \
+        lcb_subdocops_##LC(subdocops, details.index, details.flags);
+#    define PYCBC_BUILDSPEC_VAL_GEN(UC, LC, ...)              \
+        lcb_subdocops_##LC(subdocops,               \
+                           details.index,           \
+                           details.flags,           \
+                           details.pathbuf->buffer, \
+                           details.pathbuf->length, \
+                           details.valbuf->buffer,  \
+                           details.valbuf->length);
+
+#    define PYCBC_BUILDSPEC_SDCMD_CASE(UC, LC, ...) \
+        PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_BUILDSPEC_PATH_ONLY, __VA_ARGS__)
+#    define PYCBC_BUILDSPEC_SDCMD_CASE_NP(UC, LC, ...) \
+        PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_BUILDSPEC_NP, __VA_ARGS__)
+#    define PYCBC_BUILDSPEC_SDCMD_CASE_VAL(UC, LC, ...) \
+        PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_BUILDSPEC_VAL_GEN, __VA_ARGS__)
+#    define PYCBC_BUILDSPEC_SDCMD_CASE_MVAL(UC, LC, ...) \
+        PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_BUILDSPEC_VAL_GEN, __VA_ARGS__)
+#    define PYCBC_BUILDSPEC_SDCMD_CASE_COUNTER(UC, LC, ...) \
+        PYCBC_SDCMD_CASE_GENERIC(UC, LC, PYCBC_BUILDSPEC_COUNTER, __VA_ARGS__)
+    switch (details.op) {
+        PYCBC_X_SD_OPS(PYCBC_BUILDSPEC_SDCMD_CASE,
+                       PYCBC_BUILDSPEC_SDCMD_CASE_NP,
+                       PYCBC_BUILDSPEC_SDCMD_CASE_VAL,
+                       PYCBC_BUILDSPEC_SDCMD_CASE_MVAL,
+                       PYCBC_BUILDSPEC_SDCMD_CASE_COUNTER)
+        //case LCB_SDCMD_MAX:
+        //    result.err = LCB_EINVAL;
+        //    break;
+        default:
+            result.err = LCB_SUBDOC_PATH_EINVAL;
+            break;
+    }
+#else
+    lcb_SDSPEC *sdspec = &subdocops->specs[details.index];
+    sdspec->sdcmd = details.op;
+    sdspec->options = details.flags;
+
+    if (details.pathbuf && details.pathbuf->buffer && details.pathbuf->length) {
+        LCB_SDSPEC_SET_PATH(
+                sdspec, details.pathbuf->buffer, details.pathbuf->length);
+    }
+    if (details.valbuf && details.valbuf->buffer && details.valbuf->length) {
+        LCB_SDSPEC_SET_VALUE(
+                sdspec, details.valbuf->buffer, details.valbuf->length)
+    }
+#endif
+
+    return result;
+}
