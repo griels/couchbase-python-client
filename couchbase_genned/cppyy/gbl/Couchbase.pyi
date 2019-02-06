35e0ccc436098afbcbbf3042194328b8c1b4f249
Ellis Breen <ellis.breen@couchbase.com>
2019-02-06 19:32:18 +0000

    Collections front end work on V3
    Change-Id: I6d0d43ef130ed6f7c2324abed7c37c255d91982c (+1 squashed commit)
    Squashed commits:
    [8416a79] PYCBC-540: MutateInSpec support
    
    Change-Id: I765fb367fb6f65de8a5f6b7a772ec1bc65d31f13
    
    PYCBC-540: Add Basic Query for SDK 3.0
    
    Change-Id: Id95ab9d7cc437f6e8dbb4e3520afcd0f01f33da4
    (cherry picked from commit 092ef11afba4e4642bf21bbc5cc1c84506ce8d99)
    
    PYCBC-526: Implement CPPYY bindings
    
    Change-Id: I606d2fe1c348997bef6f3ef6ffe5ddf8bdd7bea6
    (cherry picked from commit 49378595c6c71cee47ca0dcd68065af6a881d204)

---
+++ couchbase_genned/cppyy/gbl/Couchbase.pyi
@@ -0,0 +1,10 @@
+from typing import Any
+
+Buffer: Any
+Client: Any
+Couchbase::Handler: Any
+Couchbase::Response<Couchbase::OpInfo::Get>: Any
+GetResponse: Any
+Handler: Any
+Response: Any
+Response<Couchbase::OpInfo::Get>: Any
