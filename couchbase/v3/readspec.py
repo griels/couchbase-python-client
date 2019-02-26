182b803c9ec86fbea895dcfb2149624d0bea095c
Ellis Breen <ellis.breen@couchbase.com>
2019-02-26 21:44:31 +0000

    SDK3 Frontend enhancements
    Change-Id: I39488db83c150ce5c9e57f2e32fb33de4db89414

---
+++ couchbase/v3/readspec.py
@@ -0,0 +1,45 @@
+import copy
+
+from couchbase.v3 import GetFullDocumentOperation, GetOperation, GetStringOperation, GetIntOperation, ExistsOperation
+
+
+class ReadSpec(list):
+    def get_full_document(self):
+        # type: ()->ReadSpec
+        result = copy.deepcopy(self)
+        result.append(GetFullDocumentOperation())
+        return result
+
+    def get(self, *path  # type: Tuple[str,...]
+            ):
+        # type: (...) -> ReadSpec
+        if not hasattr(path, "__iter__"):
+            return self.get_single(path)
+        result = copy.deepcopy(self)
+        result += map(lambda v: GetOperation(v, False), path)
+        return result
+
+    def get_single(self, path,  # type: str
+                   xattr=False  # type: bool
+                   ):
+        # type (...) -> ReadSpec
+        result = copy.deepcopy(self)
+        result.append(GetOperation(path, xattr))
+
+    def get_str(self, path,  # type: str
+                xattr=False  # type: bool
+                ):
+        result = copy.deepcopy(self)
+        result.append(GetStringOperation(path, xattr))
+
+    def get_int(self, path,  # type: str
+                xattr=False  # type: bool
+                ):
+        result = copy.deepcopy(self)
+        result.append(GetIntOperation(path, xattr))
+
+    def exists(self, path,  # type: str
+               xattr=False  # type: bool
+               ):
+        result = copy.deepcopy(self)
+        result.append(ExistsOperation(path, xattr))
