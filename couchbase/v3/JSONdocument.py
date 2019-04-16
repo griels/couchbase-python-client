9665060771e39f775e62edc2c1d4470cd2f8a971
Ellis Breen <ellis.breen@couchbase.com>
2019-04-16 18:05:56 +0100

    More frontend fixes
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: I3130f1e7fa5968f5290c4075af24dc053df1f3e0

--- couchbase/v3/JSONdocument.py
+++ couchbase/v3/JSONdocument.py
@@ -1,18 +1,24 @@
 import copy
 
 import pyrsistent
+import json
 
 
-class JSONDocument(pyrsistent.PRecord):
-    def __init__(self,
-                 parent=None,  # type: JSONDocument
+class JSONDocument(object):
+    record=None  # type: pyrsistent.PRecord
+
+    def __init__(self,parent=None,
                  **kwargs):
-        super(JSONDocument, self).__init__(parent=parent, **kwargs)
-        self.parent = parent
+        self.record=parent.record if parent else pyrsistent.PRecord()#**kwargs)
+        for k,v in kwargs.items():
+            self.record=self.record.set(k,v)
 
     def put(self, key, value):
         # type: (str, JSON) -> JSONDocument
-        result = copy.copy(self)  # type: JSONDocument
-        result.parent = self
-        result.put(key, value)
+        return JSONDocument(parent=self,**{key:value})
+
+    @classmethod
+    def decode(cls,input):
+        result=cls()
+        result.record=pyrsistent.freeze(input)
         return result
