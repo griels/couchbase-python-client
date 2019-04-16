7831ec1c81cbe0886f961296b67bf2ca1d99e680
Ellis Breen <ellis.breen@couchbase.com>
2019-04-16 18:05:56 +0100

    More frontend fixes
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: I3130f1e7fa5968f5290c4075af24dc053df1f3e0

--- couchbase/v3/result.py
+++ couchbase/v3/result.py
@@ -15,7 +15,8 @@ class ContentProxy:
                     item  # type: Type[Proxy_T]
                     ):
         # type: (...)->Proxy_T
-        return self.content
+        decode_method=getattr(item,'decode',None)
+        return decode_method(self.content) if decode_method else item(self.content)
 
 
 class IResult:
@@ -109,7 +110,7 @@ class GetResult(IGetResult):
 
 
 def get_result(self, x, options=None):
-    options|={}
+    options = options or {}
     return GetResult(x.value, cas=x.cas, expiry=options.pop('timeout', None), id=x.key)
 
 
