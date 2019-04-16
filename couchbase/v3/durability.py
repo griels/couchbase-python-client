b78855b7b8f427f2101680e3a28ecb47ee09d751
Ellis Breen <ellis.breen@couchbase.com>
2019-04-16 18:05:56 +0100

    PYCBC-540:Add Basic CRUD for SDK 3.0 PYCBC-541:Add Basic Query for SDK 3.0 More frontend fixes
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: I3130f1e7fa5968f5290c4075af24dc053df1f3e0

---
+++ couchbase/v3/durability.py
@@ -0,0 +1,35 @@
+from couchbase.v3.options import OptionBlock
+
+
+class Value(int):
+    pass
+    # def __init__(self,
+    #             value  # type: Union[str,bytes,SupportsInt]
+    #             ):
+    #    super(Value, self).__init__(value)
+
+
+class Cardinal(OptionBlock):
+    Value = Value
+
+    class ONE(Value):
+        def __init__(self, *args, **kwargs):
+            super(Cardinal.ONE, self).__init__(1)
+
+    TWO = Value(2)
+    THREE = Value(3)
+    NONE = Value(0)
+
+
+class ReplicateTo(Cardinal):
+    pass
+
+
+class PersistTo(Cardinal):
+    pass
+
+
+class Durability(object):
+    MajorityAndPersistActive = None
+
+
