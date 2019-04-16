e6bb76a03061dbc1c9182ce7a396631ee94b99ca
Ellis Breen <ellis.breen@couchbase.com>
2019-04-16 18:05:56 +0100

    More frontend fixes
    SDKJENKINS_PIP_INSTALL
    PYCBC_ASSERT_CONTINUE
    SDKJENKINS_LINUX
    
    Change-Id: I3130f1e7fa5968f5290c4075af24dc053df1f3e0

--- couchbase/v3/mutate_in.py
+++ couchbase/v3/mutate_in.py
@@ -43,3 +43,39 @@ class MutateInSpecItem(enum.Enum):
 
 
 MutateInSpec = Tuple[MutateInSpecItem, ...]
+
+
+class MutationToken(object):
+    def __init__(self, sequenceNumber,  # type: int
+                 vbucketId,  # type: int
+                 vbucketUUID  # type: int
+                 ):
+        self.sequenceNumber = sequenceNumber
+        self.vbucketId = vbucketId
+        self.vbucketUUID = vbucketUUID
+
+    def partition_id(self):
+        # type: (...)->int
+        pass
+
+    def partition_uuid(self):
+        # type: (...)->int
+        pass
+
+    def sequence_number(self):
+        # type: (...)->int
+        pass
+
+    def bucket_name(self):
+        # type: (...)->str
+        pass
+
+
+class SDK2MutationToken(MutationToken):
+    def __init__(self, token):
+        token=token or (None,None,None)
+        super(SDK2MutationToken,self).__init__(token[2],token[0],token[1])
+            #LCB_MUTATION_TOKEN_VB(mutinfo),
+            #LCB_MUTATION_TOKEN_ID(mutinfo),
+            #LCB_MUTATION_TOKEN_SEQ(mutinfo),
+            #conn->bucket);
