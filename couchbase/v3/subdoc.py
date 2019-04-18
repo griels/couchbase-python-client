7d9c8c88dbf20a2b2800871fe2443cea8b434bff
Ellis Breen <ellis.breen@couchbase.com>
2019-04-18 11:38:28 +0100

    Collections front end work on V3
    Change-Id: I6d0d43ef130ed6f7c2324abed7c37c255d91982c (+1 squashed commit)
    Squashed commits:
    [8416a79] PYCBC-540: MutateInSpec support
    
    Change-Id: I765fb367fb6f65de8a5f6b7a772ec1bc65d31f13
    
    PYCBC-540: Add Basic Query for SDK 3.0
    
    Change-Id: Id95ab9d7cc437f6e8dbb4e3520afcd0f01f33da4
    (cherry picked from commit 092ef11afba4e4642bf21bbc5cc1c84506ce8d99)
    
    PYCBC-526: Implement CPPYY bindings
    
    Change-Id: I606d2fe1c348997bef6f3ef6ffe5ddf8bdd7bea6
    (cherry picked from commit 49378595c6c71cee47ca0dcd68065af6a881d204)

---
+++ couchbase/v3/subdoc.py
@@ -0,0 +1,69 @@
+from couchbase.result import ValueResult
+from couchbase import subdocument as SD
+
+SDType = type(SD)
+Document = ValueResult
+
+
+# noinspection PyPep8Naming
+def GetSpec():
+    # type: ()-> SDType
+    return SD
+
+
+# noinspection PyPep8Naming
+def LookupInSpec():
+    # type: ()-> SDType
+    return SD
+
+
+# noinspection PyPep8Naming
+def MutateSpec():
+    # type: ()-> SDType
+    return SD
+
+"""
+  MutateInOp.replace('some.path', {xattr: '_sync'}),
+  MutateInOp.insert('some.other.path`, {xattr: '_sync', createParents: true})
+], ...)"""
+
+
+def LookupOperation(*args, **kwargs):
+    return super(LookupOperation, cls).__new__(cls, tuple(args))
+
+import couchbase.subdocument as SD
+
+def GetOperation(path,  # type: str
+                 xattr  # type: bool
+                 ):
+        return SD.get(path,xattr=xattr)
+        #super(GetOperation,cls).__new__(SD.get(path,xattr=xattr))
+
+
+def GetFullDocumentOperation(path,  # type: str
+                 xattr  # type: bool
+                 ):
+    return SD.get(path,xattr=xattr)
+
+#
+# class GetStringOperation(LookupOperation):
+#     def __init__(self,
+#                  path,  # type: str
+#                  xattr  # type: bool
+#                  ):
+#         super(GetOperation,self).__init__(SD.get(path,xattr=xattr))
+#
+#
+# class GetIntOperation(LookupOperation):
+#     def __init__(self,
+#                  path,  # type: str
+#                  xattr  # type: bool
+#                  ):
+#         super(GetOperation,self).__init__(SD.get(path,xattr=xattr))
+#
+#
+def ExistsOperation(
+                 path,  # type: str
+                 xattr  # type: bool
+                 ):
+        return SD.exists(path,xattr=xattr)
