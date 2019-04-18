f59c72c13a005ae0bf94ff719218888cb51371c5
Ellis Breen <ellis.breen@couchbase.com>
2019-04-18 11:38:28 +0100

    Collections front end work on V3
    Change-Id: I6d0d43ef130ed6f7c2324abed7c37c255d91982c (+1 squashed commit)
    Squashed commits:
    [8416a79] PYCBC-540: MutateInSpec support
    
    Change-Id: I765fb367fb6f65de8a5f6b7a772ec1bc65d31f13
    
    PYCBC-540: Add Basic Query for SDK 3.0
    
    Change-Id: Id95ab9d7cc437f6e8dbb4e3520afcd0f01f33da4
    (cherry picked from commit 092ef11afba4e4642bf21bbc5cc1c84506ce8d99)
    
    PYCBC-526: Implement CPPYY bindings
    
    Change-Id: I606d2fe1c348997bef6f3ef6ffe5ddf8bdd7bea6
    (cherry picked from commit 49378595c6c71cee47ca0dcd68065af6a881d204)

---
+++ couchbase/v3/mutate_in.py
@@ -0,0 +1,80 @@
+import enum
+from typing import *
+
+
+class MutateInSpecItemBase(object):
+    def __init__(self,
+                 path,  # type: str
+                 xattr=None,
+                 createParents=None,
+                 options=None  # type: MutateOptions
+                 ):
+        # type: (...)->None
+        self.path = path
+        self.options = options
+
+
+class MutateReplace(MutateInSpecItemBase):
+    def __init__(self, *args, **kwargs):
+        super(MutateReplace, self).__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        return
+
+    fish = 0
+
+
+class MutateInsert(MutateInSpecItemBase):
+    def __init__(self, *args, **kwargs):
+        super(MutateInsert, self).__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        return
+
+    cheese = 9
+
+
+class MutateInSpecItem(enum.Enum):
+    replace = MutateReplace
+    insert = MutateInsert
+
+    def __call__(self, *args, **kwargs):
+        return self.value(*args, **kwargs)
+
+
+MutateInSpec = Tuple[MutateInSpecItem, ...]
+
+
+class MutationToken:
+    def __init__(self, sequenceNumber,  # type: int
+                 vbucketId,  # type: int
+                 vbucketUUID  # type: int
+                 ):
+        self.sequenceNumber = sequenceNumber
+        self.vbucketId = vbucketId
+        self.vbucketUUID = vbucketUUID
+
+    def partition_id(self):
+        # type: (...)->int
+        pass
+
+    def partition_uuid(self):
+        # type: (...)->int
+        pass
+
+    def sequence_number(self):
+        # type: (...)->int
+        pass
+
+    def bucket_name(self):
+        # type: (...)->str
+        pass
+
+
+class SDK2MutationToken(MutationToken):
+    def __init__(self, token):
+        super(SDK2MutationToken,self).__init__(token[2],token[0],token[1])
+            #LCB_MUTATION_TOKEN_VB(mutinfo),
+            #LCB_MUTATION_TOKEN_ID(mutinfo),
+            #LCB_MUTATION_TOKEN_SEQ(mutinfo),
+            #conn->bucket);
